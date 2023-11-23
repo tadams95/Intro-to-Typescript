2ea2cd801284b61fe41dbeb5bf55c9f04ba2b1cf
tadams95 <tyrelleadams28@gmail.com>
2023-11-22 17:05:29 -0800

    Manage Timer Interval with Ref

--- adv-components/src/App.tsx
+++ adv-components/src/App.tsx
@@ -1,26 +1,17 @@
-import Button from "./components/Button";
-import Input from "./components/Input";
-import Form, { type FormHandle } from "./components/Form";
-import { useRef } from "react";
+import AddTimer from "./components/AddTimer";
+import Header from "./components/Header";
+import Timers from "./components/Timers";
+import TimersContextProvider from "./store/timers-context";
 
 function App() {
-  const customForm = useRef<FormHandle>(null);
-
-  function handleSave(data: unknown) {
-    const extractedData = data as { name: string; age: string };
-    console.log(extractedData);
-    customForm.current?.clear();
-  }
   return (
-    <main>
-      <Form onSave={handleSave} ref={customForm}>
-        <Input type="text" label="Name" id="name" />
-        <Input type="number" label="Age" id="age" />
-        <p>
-          <Button>Save</Button>
-        </p>
-      </Form>
-    </main>
+    <TimersContextProvider>
+      <Header />
+      <main>
+        <AddTimer />
+        <Timers />
+      </main>
+    </TimersContextProvider>
   );
 }
 
