bbefd69401dfbfb935b7de5ded3696ab69754be2
tadams95 <tyrelleadams28@gmail.com>
2023-11-23 13:19:17 -0800

    redux and typescript

---
+++ redux-ts/src/store/hooks.ts
@@ -0,0 +1,8 @@
+import { TypedUseSelectorHook, useDispatch, useSelector } from "react-redux";
+import { AppDispatch, RootState } from "./store";
+
+type DispatchFunction = () => AppDispatch;
+
+export const useCartDispatch: DispatchFunction = useDispatch;
+
+export const useCartSelector: TypedUseSelectorHook<RootState> = useSelector;
